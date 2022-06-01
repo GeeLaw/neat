8aafb91c60f343d17cd473e4e3e80dd8441e0a65
Gee Law <geelaw@outlook.com>
2022-06-01 01:25:57 -0400

    Add nullable primitive comparer.

--- src/Neat/Comparers/PrimitiveComparer.cs
+++ src/Neat/Comparers/PrimitiveComparer.cs
@@ -21,7 +21,22 @@ namespace Neat.Comparers
       IEqualityComparer2<ulong>, IComparer<ulong>,
       IEqualityComparer2<short>, IComparer<short>,
       IEqualityComparer2<ushort>, IComparer<ushort>,
-      IEqualityComparer2<string>, IComparer<string>
+      IEqualityComparer2<string>, IComparer<string>,
+      IEqualityComparer2<bool?>, IComparer<bool?>,
+      IEqualityComparer2<byte?>, IComparer<byte?>,
+      IEqualityComparer2<sbyte?>, IComparer<sbyte?>,
+      IEqualityComparer2<char?>, IComparer<char?>,
+      IEqualityComparer2<decimal?>, IComparer<decimal?>,
+      IEqualityComparer2<double?>, IComparer<double?>,
+      IEqualityComparer2<float?>, IComparer<float?>,
+      IEqualityComparer2<int?>, IComparer<int?>,
+      IEqualityComparer2<uint?>, IComparer<uint?>,
+      IEqualityComparer2<nint?>, IComparer<nint?>,
+      IEqualityComparer2<nuint?>, IComparer<nuint?>,
+      IEqualityComparer2<long?>, IComparer<long?>,
+      IEqualityComparer2<ulong?>, IComparer<ulong?>,
+      IEqualityComparer2<short?>, IComparer<short?>,
+      IEqualityComparer2<ushort?>, IComparer<ushort?>
   {
     public static PrimitiveComparer Instance
     {
@@ -38,7 +53,7 @@ namespace Neat.Comparers
     [MethodImpl(Helper.OptimizeInline)]
     public int GetHashCode(bool obj)
     {
-      return obj ? 0 : 1;
+      return obj ? -1 : 1;
     }
 
     [MethodImpl(Helper.OptimizeInline)]
@@ -339,6 +354,357 @@ namespace Neat.Comparers
     public int Compare(string x, string y)
     {
       return string.CompareOrdinal(x, y);
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(bool? x, bool? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() ^ y.GetValueOrDefault());
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(bool? obj)
+    {
+      return obj.HasValue
+        ? obj.GetValueOrDefault()
+          ? -1
+          : 1
+        : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(bool? x, bool? y)
+    {
+      return (x.HasValue ? x.GetValueOrDefault() ? 2 : 1 : 0)
+        - (y.HasValue ? y.GetValueOrDefault() ? 2 : 1 : 0);
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(byte? x, byte? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() == y.GetValueOrDefault());
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(byte? obj)
+    {
+      return obj.GetValueOrDefault();
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(byte? x, byte? y)
+    {
+      return (x.HasValue ? x.GetValueOrDefault() : -131072)
+        - (y.HasValue ? y.GetValueOrDefault() : -131072);
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(sbyte? x, sbyte? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() == y.GetValueOrDefault());
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(sbyte? obj)
+    {
+      return obj.GetValueOrDefault();
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(sbyte? x, sbyte? y)
+    {
+      return (x.HasValue ? x.GetValueOrDefault() : -131072)
+        - (y.HasValue ? y.GetValueOrDefault() : -131072);
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(char? x, char? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() == y.GetValueOrDefault());
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(char? obj)
+    {
+      return obj.GetValueOrDefault();
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(char? x, char? y)
+    {
+      return (x.HasValue ? x.GetValueOrDefault() : -131072)
+        - (y.HasValue ? y.GetValueOrDefault() : -131072);
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(decimal? x, decimal? y)
+    {
+      return x.HasValue
+        ? y.HasValue && decimal.Equals(x.GetValueOrDefault(), y.GetValueOrDefault())
+        : !y.HasValue;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(decimal? obj)
+    {
+      return obj.HasValue ? obj.GetValueOrDefault().GetHashCode() : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(decimal? x, decimal? y)
+    {
+      return x.HasValue
+        ? y.HasValue
+          ? decimal.Compare(x.GetValueOrDefault(), y.GetValueOrDefault())
+          : 1
+        : y.HasValue
+          ? -1
+          : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(double? x, double? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault().Equals(y.GetValueOrDefault()));
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(double? obj)
+    {
+      return obj.HasValue ? obj.GetValueOrDefault().GetHashCode() : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(double? x, double? y)
+    {
+      return x.HasValue
+        ? y.HasValue
+          ? x.GetValueOrDefault().CompareTo(y.GetValueOrDefault())
+          : 1
+        : y.HasValue
+          ? -1
+          : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(float? x, float? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault().Equals(y.GetValueOrDefault()));
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(float? obj)
+    {
+      return obj.HasValue ? obj.GetValueOrDefault().GetHashCode() : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(float? x, float? y)
+    {
+      return x.HasValue
+        ? y.HasValue
+          ? x.GetValueOrDefault().CompareTo(y.GetValueOrDefault())
+          : 1
+        : y.HasValue
+          ? -1
+          : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(int? x, int? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() == y.GetValueOrDefault());
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(int? obj)
+    {
+      return obj.GetValueOrDefault();
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(int? x, int? y)
+    {
+      return x.HasValue
+        ? y.HasValue
+          ? Compare(x.GetValueOrDefault(), y.GetValueOrDefault())
+          : 1
+        : y.HasValue
+          ? -1
+          : 0;
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(uint? x, uint? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() == y.GetValueOrDefault());
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(uint? obj)
+    {
+      return (int)obj.GetValueOrDefault();
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(uint? x, uint? y)
+    {
+      return x.HasValue
+        ? y.HasValue
+          ? Compare(x.GetValueOrDefault(), y.GetValueOrDefault())
+          : 1
+        : y.HasValue
+          ? -1
+          : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(nint? x, nint? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() == y.GetValueOrDefault());
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(nint? obj)
+    {
+      return obj.HasValue ? obj.GetValueOrDefault().GetHashCode() : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(nint? x, nint? y)
+    {
+      return x.HasValue
+        ? y.HasValue
+          ? Compare(x.GetValueOrDefault(), y.GetValueOrDefault())
+          : 1
+        : y.HasValue
+          ? -1
+          : 0;
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(nuint? x, nuint? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() == y.GetValueOrDefault());
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(nuint? obj)
+    {
+      return obj.HasValue ? obj.GetValueOrDefault().GetHashCode() : 0;
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(nuint? x, nuint? y)
+    {
+      return x.HasValue
+        ? y.HasValue
+          ? Compare(x.GetValueOrDefault(), y.GetValueOrDefault())
+          : 1
+        : y.HasValue
+          ? -1
+          : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(long? x, long? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() == y.GetValueOrDefault());
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(long? obj)
+    {
+      return obj.HasValue ? obj.GetValueOrDefault().GetHashCode() : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(long? x, long? y)
+    {
+      return x.HasValue
+        ? y.HasValue
+          ? Compare(x.GetValueOrDefault(), y.GetValueOrDefault())
+          : 1
+        : y.HasValue
+          ? -1
+          : 0;
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(ulong? x, ulong? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() == y.GetValueOrDefault());
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(ulong? obj)
+    {
+      return obj.HasValue ? obj.GetValueOrDefault().GetHashCode() : 0;
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(ulong? x, ulong? y)
+    {
+      return x.HasValue
+        ? y.HasValue
+          ? Compare(x.GetValueOrDefault(), y.GetValueOrDefault())
+          : 1
+        : y.HasValue
+          ? -1
+          : 0;
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(short? x, short? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() == y.GetValueOrDefault());
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(short? obj)
+    {
+      return obj.GetValueOrDefault();
+    }
+
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(short? x, short? y)
+    {
+      return (x.HasValue ? x.GetValueOrDefault() : -131072)
+        - (y.HasValue ? y.GetValueOrDefault() : -131072);
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public bool Equals(ushort? x, ushort? y)
+    {
+      return (x.HasValue ^ y.HasValue) & (x.GetValueOrDefault() == y.GetValueOrDefault());
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public int GetHashCode(ushort? obj)
+    {
+      return obj.GetValueOrDefault();
+    }
+
+    [CLSCompliant(false)]
+    [MethodImpl(Helper.OptimizeInline)]
+    public int Compare(ushort? x, ushort? y)
+    {
+      return (x.HasValue ? x.GetValueOrDefault() : -131072)
+        - (y.HasValue ? y.GetValueOrDefault() : -131072);
     }
   }
 }
