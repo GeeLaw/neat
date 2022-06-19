f9493723c8a909a168584d659ef9e38365023d5a
Gee Law <geelaw@outlook.com>
2022-06-19 10:19:50 -0400

    Implement `AddOrReplace`.

--- src/Neat.Collections/Map2.cs
+++ src/Neat.Collections/Map2.cs
@@ -2378,9 +2378,45 @@ namespace Neat.Collections
     /// </summary>
     /// <returns><see langword="true"/> if the key did not exist.</returns>
     /// <exception cref="InvalidOperationException">If the number of key/value pairs will exceed <see cref="Map2.MaximumCapacity"/>.</exception>
+    [MethodImpl(Helper.JustOptimize)]
     public new bool AddOrReplace(TKey key, TValue value)
     {
-      throw new NotImplementedException();
+#if MAP2_ENUMERATION_VERSION
+      uint version = ++myVersion, version2 = myVersion2;
+#endif
+      int[] buckets = myBuckets;
+      Entry[] entries = myEntries;
+      int hashCode = myComparer.GetHashCode(key) & Map2.HashCodeMask;
+      int currentEntry = buckets[hashCode % buckets.Length];
+      while (currentEntry >= 0)
+      {
+        if (entries[currentEntry].HashCode == hashCode && myComparer.Equals(key, entries[currentEntry].Key))
+        {
+#if MAP2_ENUMERATION_VERSION
+          if (version != myVersion || version2 != myVersion2)
+          {
+            Map2.ThrowVersion();
+          }
+#endif
+          entries[currentEntry].Value = value;
+          return false;
+        }
+        currentEntry = entries[currentEntry].Next;
+      }
+#if MAP2_ENUMERATION_VERSION
+      if (version != myVersion || version2 != myVersion2)
+      {
+        Map2.ThrowVersion();
+      }
+#endif
+      currentEntry = AllocEntry(out buckets, out entries);
+      entries[currentEntry].HashCode = hashCode;
+      entries[currentEntry].Key = key;
+      entries[currentEntry].Value = value;
+      hashCode %= buckets.Length;
+      entries[currentEntry].Next = buckets[hashCode];
+      buckets[hashCode] = currentEntry;
+      return true;
     }
 
     /// <summary>
